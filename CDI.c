9b3790d1b2a47b923cd84f0dce0ccc325a0d9933
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-17 10:47:42 -0700

    compile fix?

--- CDI.c
+++ CDI.c
@@ -36,7 +36,7 @@ static void activate(GtkApplication* app, gpointer user_data) {
 
 int main(int argc, char **argv) {
     // Initialize GTK application
-    GtkApplication *app = gtk_application_new("org.example.crt_terminal", G_APPLICATION_FLAGS_NONE);
+    GtkApplication *app = gtk_application_new("org.example.CDI", G_APPLICATION_DEFAULT_FLAGS);
     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
     int status = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);
