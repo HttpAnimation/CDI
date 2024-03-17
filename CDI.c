d20dda52f85e6e601b728b9e30cef391484305c2
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-17 10:31:37 -0700

    real gui

--- CDI.c
+++ CDI.c
@@ -1,49 +1,89 @@
 #include <QApplication>
+#include <QMainWindow>
+#include <QVBoxLayout>
 #include <QProcess>
+#include <QPlainTextEdit>
+#include <QLineEdit>
+#include <QPushButton>
+
+class TerminalWindow : public QMainWindow {
+  Q_OBJECT
+
+public:
+  TerminalWindow(QWidget *parent = nullptr);
+
+private slots:
+  void onCommandEntered();
+
+private:
+  QVBoxLayout *layout;
+  QPlainTextEdit *outputText;
+  QLineEdit *inputLine;
+  QProcess *process;
+};
+
+TerminalWindow::TerminalWindow(QWidget *parent) : QMainWindow(parent) {
+  // Create main layout
+  layout = new QVBoxLayout;
+  setLayout(layout);
+
+  // Output text box
+  outputText = new QPlainTextEdit();
+  outputText->setReadOnly(true);
+  layout->addWidget(outputText);
+
+  // Input line
+  inputLine = new QLineEdit;
+  layout->addWidget(inputLine);
+
+  // Run button
+  QPushButton *runButton = new QPushButton("Run");
+  connect(runButton, &QPushButton::clicked, this, &TerminalWindow::onCommandEntered);
+  layout->addWidget(runButton);
+
+  // Process object
+  process = new QProcess;
+}
+
+void TerminalWindow::onCommandEntered() {
+  QString command = inputLine->text();
+  if (command.isEmpty()) {
+    return;
+  }
+
+  // Clear previous output
+  outputText->clear();
+
+  // Split command and arguments
+  QStringList arguments = command.split(" ", QString::SkipEmptyParts);
+
+  // Set program and arguments
+  process->setProgram(arguments[0]);
+  if (arguments.size() > 1) {
+    process->setArguments(arguments.mid(1));
+  }
+
+  // Connect signals for capturing output
+  connect(process, &QProcess::readyReadStandardOutput(), this, [&]() {
+    outputText->appendPlainText(process->readAllStandardOutput());
+  });
+
+  connect(process, &QProcess::readyReadStandardError(), this, [&]() {
+    outputText->appendPlainText(process->readAllStandardError());
+  });
+
+  // Start process
+  process->start();
+
+  // Clear input line after process starts
+  inputLine->clear();
+}
 
 int main(int argc, char *argv[]) {
   QApplication app(argc, argv);
-
-  // Get the command to execute (if any) from program arguments
-  QString command;
-  if (argc > 1) {
-    command = argv[1];
-  } else {
-    qWarning("No command provided. Please specify a command to execute.");
-    return 1;
-  }
-
-  // Create a QProcess object
-  QProcess process;
-
-  // Split the command and arguments (if any)
-  QStringList arguments = command.split(" ", QString::SkipEmptyParts);
-
-  // Set the program to execute
-  process.setProgram(arguments[0]);
-
-  // Set any additional arguments (if present)
-  if (arguments.size() > 1) {
-    process.setArguments(arguments.mid(1));
-  }
-
-  // Connect signals for monitoring process execution (optional)
-  QObject::connect(&process, SIGNAL(readyReadStandardOutput()), &process, SLOT(readAllStandardOutput()));
-  QObject::connect(&process, SIGNAL(readyReadStandardError()), &process, SLOT(readAllStandardError()));
-
-  // Start the process
-  process.start();
-
-  // Wait for the process to finish
-  process.waitForFinished();
-
-  // Print the output (optional)
-  if (process.exitStatus() == QProcess::NormalExit) {
-    qDebug() << "Standard Output:\n" << process.readAllStandardOutput();
-  } else {
-    qWarning() << "Error: Process failed with exit code" << process.exitStatus();
-    qDebug() << "Standard Error:\n" << process.readAllStandardError();
-  }
-
-  return 0;
+  TerminalWindow window;
+  window.show();
+  return app.exec();
 }
+
+#include "terminalwindow.h"
