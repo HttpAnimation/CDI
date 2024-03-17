450511405edb5d61c7ca5f0e0896398c5ff83045
HttpAnimation <shawndouglasyt@gmail.com>
2024-03-17 10:38:08 -0700

    gtk port

--- CDI.c
+++ CDI.c
@@ -1,89 +1,48 @@
-#include <QApplication>
-#include <QMainWindow>
-#include <QVBoxLayout>
-#include <QProcess>
-#include <QPlainTextEdit>
-#include <QLineEdit>
-#include <QPushButton>
+#include <gtk/gtk.h>
 
-class TerminalWindow : public QMainWindow {
-  Q_OBJECT
-
-public:
-  TerminalWindow(QWidget *parent = nullptr);
-
-private slots:
-  void onCommandEntered();
-
-private:
-  QVBoxLayout *layout;
-  QPlainTextEdit *outputText;
-  QLineEdit *inputLine;
-  QProcess *process;
-};
-
-TerminalWindow::TerminalWindow(QWidget *parent) : QMainWindow(parent) {
-  // Create main layout
-  layout = new QVBoxLayout;
-  setLayout(layout);
-
-  // Output text box
-  outputText = new QPlainTextEdit();
-  outputText->setReadOnly(true);
-  layout->addWidget(outputText);
-
-  // Input line
-  inputLine = new QLineEdit;
-  layout->addWidget(inputLine);
-
-  // Run button
-  QPushButton *runButton = new QPushButton("Run");
-  connect(runButton, &QPushButton::clicked, this, &TerminalWindow::onCommandEntered);
-  layout->addWidget(runButton);
-
-  // Process object
-  process = new QProcess;
+// Simulate terminal output (replace with actual functionality)
+void append_to_output(GtkWidget *textview, const gchar *text) {
+    GtkTextBuffer *buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(textview));
+    gtk_text_buffer_get_end_iter(buffer);
+    gtk_text_buffer_insert_at_cursor(buffer, text, -1);
 }
 
-void TerminalWindow::onCommandEntered() {
-  QString command = inputLine->text();
-  if (command.isEmpty()) {
-    return;
-  }
-
-  // Clear previous output
-  outputText->clear();
-
-  // Split command and arguments
-  QStringList arguments = command.split(" ", QString::SkipEmptyParts);
-
-  // Set program and arguments
-  process->setProgram(arguments[0]);
-  if (arguments.size() > 1) {
-    process->setArguments(arguments.mid(1));
-  }
-
-  // Connect signals for capturing output
-  connect(process, &QProcess::readyReadStandardOutput(), this, [&]() {
-    outputText->appendPlainText(process->readAllStandardOutput());
-  });
-
-  connect(process, &QProcess::readyReadStandardError(), this, [&]() {
-    outputText->appendPlainText(process->readAllStandardError());
-  });
-
-  // Start process
-  process->start();
-
-  // Clear input line after process starts
-  inputLine->clear();
+gboolean on_key_press (GtkWidget *widget, GdkEventKey *event) {
+    if (event->keyval == GDK_KEY_Return) {
+        // Process user input (replace with actual command execution)
+        gchar *input_text = gtk_entry_get_text(GTK_ENTRY(widget));
+        g_print("User input: %s\n", input_text);
+        append_to_output(gtk_text_view_get_parent(GTK_TEXT_VIEW(widget)), "Simulated output\n");
+        gtk_entry_set_text(GTK_ENTRY(widget), "");
+    }
+    return TRUE;
 }
 
 int main(int argc, char *argv[]) {
-  QApplication app(argc, argv);
-  TerminalWindow window;
-  window.show();
-  return app.exec();
-}
+    gtk_init(&argc, &argv);
 
-#include "terminalwindow.h"
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Terminal Emulator");
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+    gtk_container_add(GTK_CONTAINER(window), vbox);
+
+    GtkWidget *textview = gtk_text_view_new();
+    gtk_text_view_set_editable(GTK_TEXT_VIEW(textview), FALSE);
+    gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(textview), GTK_WRAP_WORD);
+    gtk_box_pack_start(GTK_BOX(vbox), textview, TRUE, TRUE, 0);
+
+    GtkWidget *scrolled_window = gtk_scrolled_window_new(NULL, NULL);
+    gtk_container_add(GTK_CONTAINER(scrolled_window), textview);
+    gtk_box_pack_start(GTK_BOX(vbox), scrolled_window, TRUE, TRUE, 0);
+
+    GtkWidget *entry = gtk_entry_new();
+    g_signal_connect(entry, "key-press-event", G_CALLBACK(on_key_press), NULL);
+    gtk_box_pack_start(GTK_BOX(vbox), entry, FALSE, FALSE, 0);
+
+    gtk_widget_show_all(window);
+    gtk_main();
+
+    return 0;
+}
